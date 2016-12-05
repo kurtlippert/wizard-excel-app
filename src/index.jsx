e4efef36cee0f90c8e3ac900f56cd2062e7048fb
Kurt Lippert <klippert@broadvine.com>
2016-12-05 10:33:03 -0500

    removed unnecessary code

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-// injectTapEventPlugin();
-
 import 'semantic-ui-css/semantic.css';
-import './main.scss';
 
 // Components
 import Hello from './components/Hello';
 import NewButton from './components/Button';
 
-// Styles
-// import './main.css';
-
 function App() {
     return (
         <div>
-            <Hello />
+            <Hello compiler="Babel" framework="React" />
             <NewButton />
         </div>
     );
