b261da309f28e46b70e847b95d166739b17caa0f
Kurt Lippert <klippert@broadvine.com>
2017-01-17 10:54:30 -0500

    misc

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,9 +1,244 @@
 module Main exposing (..)
 
-import Html exposing (text)
-import Hello.World exposing (hello)
+import Html exposing (Html, input, div, button, text, h1, br, i)
+import Html.Attributes exposing (placeholder, style, type_, classList)
+import Html.Events exposing (onInput, onClick)
+import Regex exposing (regex)
+import Random
 
 
-main : Html.Html a
+main : Program Never Model Msg
 main =
-    text hello
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , age : String
+    , validationMsg : ValidationMsg
+    , dieFace : DieFace
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "" "" "" "" Empty WorldIcon, Cmd.none )
+
+
+type ValidationMsg
+    = Empty
+    | Error String
+    | OK
+
+
+type DieFace
+    = WorldIcon
+    | MusicIcon
+    | GithubIcon
+    | HeartIcon
+    | ForkIcon
+    | CloudIcon
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+    | Age String
+    | Submit
+    | Roll
+    | NewFace Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Name name ->
+            ( { model | name = name }, Cmd.none )
+
+        Password password ->
+            ( { model | password = password }, Cmd.none )
+
+        PasswordAgain password ->
+            ( { model | passwordAgain = password }, Cmd.none )
+
+        Age age ->
+            ( { model | age = age }, Cmd.none )
+
+        Submit ->
+            ( { model | validationMsg = validate model }, Cmd.none )
+
+        Roll ->
+            ( model, Random.generate NewFace (Random.int 1 6) )
+
+        NewFace newFace ->
+            ( { model | dieFace = dieFace newFace }, Cmd.none )
+
+
+validate : Model -> ValidationMsg
+validate model =
+    if String.length model.password < 9 then
+        Error "Password must be longer than 8 characters"
+    else if not (Regex.contains (regex "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$") model.password) then
+        Error "Password must be mixed case"
+    else if model.password /= model.passwordAgain then
+        Error "Passwords do not match!"
+    else if Result.withDefault 0 (String.toInt model.age) == 0 then
+        Error "Age must be a number > 0"
+    else
+        OK
+
+
+dieFace : Int -> DieFace
+dieFace newFace =
+    case newFace of
+        1 ->
+            WorldIcon
+
+        2 ->
+            MusicIcon
+
+        3 ->
+            GithubIcon
+
+        4 ->
+            HeartIcon
+
+        5 ->
+            ForkIcon
+
+        6 ->
+            CloudIcon
+
+        _ ->
+            WorldIcon
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div
+            [ classList
+                [ ( "input", True )
+                , ( "ui", True )
+                ]
+            ]
+            [ input [ type_ "text", placeholder "Name", onInput Name ] [] ]
+        , div
+            [ classList
+                [ ( "input", True )
+                , ( "ui", True )
+                ]
+            ]
+            [ input [ type_ "password", placeholder "Password", onInput Password ] [] ]
+        , div
+            [ classList
+                [ ( "input", True )
+                , ( "ui", True )
+                ]
+            ]
+            [ input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] [] ]
+        , div
+            [ classList
+                [ ( "input", True )
+                , ( "ui", True )
+                ]
+            ]
+            [ input [ type_ "text", placeholder "Age", onInput Age ] [] ]
+        , div []
+            [ button
+                [ classList
+                    [ ( "button", True )
+                    , ( "ui", True )
+                    ]
+                , onClick Submit
+                ]
+                [ text "Submit" ]
+            ]
+        , viewValidation model
+        , br [] []
+        , br [] []
+        , dieFaceRender model.dieFace
+        , div []
+            [ button
+                [ classList
+                    [ ( "button", True )
+                    , ( "ui", True )
+                    ]
+                , onClick Roll
+                ]
+                [ text "Roll" ]
+            ]
+        ]
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    let
+        ( color, message ) =
+            case model.validationMsg of
+                Empty ->
+                    ( "", "" )
+
+                Error message ->
+                    ( "red", message )
+
+                OK ->
+                    ( "green", "OK" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
+
+
+dieFaceRender : DieFace -> Html msg
+dieFaceRender dieFace =
+    let
+        iconType : String
+        iconType =
+            case dieFace of
+                WorldIcon ->
+                    "world"
+
+                MusicIcon ->
+                    "music"
+
+                GithubIcon ->
+                    "github"
+
+                HeartIcon ->
+                    "heart"
+
+                ForkIcon ->
+                    "fork"
+
+                CloudIcon ->
+                    "cloud"
+    in
+        i [ classList [ ( iconType, True ), ( "icon", True ) ] ] []
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-var path              = require( 'path' );
-var webpack           = require( 'webpack' );
-var merge             = require( 'webpack-merge' );
-var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-var PurifyCSSPlugin   = require( 'purifycss-webpack-plugin' );
-var OptimizeCSSPlugin = require( 'optimize-css-assets-webpack-plugin' );
-var CleanDistPlugin   = require( 'clean-webpack-plugin' ); 
-var entryPath         = path.join( __dirname, 'src/static/index.js' );
-var outputPath        = path.join( __dirname, 'dist' );
+const path              = require( 'path' );
+const webpack           = require( 'webpack' );
+const merge             = require( 'webpack-merge' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const PurifyCSSPlugin   = require( 'purifycss-webpack-plugin' );
+const OptimizeCSSPlugin = require( 'optimize-css-assets-webpack-plugin' );
+const CleanDistPlugin   = require( 'clean-webpack-plugin' ); 
+const entryPath         = path.join( __dirname, 'src/static/index.js' );
+const outputPath        = path.join( __dirname, 'dist' );
 
 console.log( 'WEBPACK GO!');
 
 // determine build env
-var TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development';
-var outputFilename = TARGET_ENV === 'production' ? '[name].[hash].js' : '[name].js'
+const TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development';
+const outputFilename = TARGET_ENV === 'production' ? '[name].[hash].js' : '[name].js';
+const vendorCSSName = TARGET_ENV === 'production' ? 'vendor.[contenthash].css' : 'vendor.css';
 
 // common webpack config
-var commonConfig = {
+const commonConfig = {
 
   output: {
     path:       outputPath,
@@ -30,29 +31,42 @@ var commonConfig = {
   module: {
     noParse: /\.elm$/,
     loaders: [
-      {
-        test: /\.css$/i,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: 'css-loader',
-        }),
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2|svg)$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=25000',
-      },
-      {
-        test: /\.(woff|woff2|ttf|eot)$/,
-        loader: 'url-loader?limit=50000',
-      },
-      {
-        test: /\.svg$/,
-        loader: 'file-loader',
-      },
+//      {
+//        test:   /\.(eot|ttf|woff|woff2|svg)$/,
+//        loader: 'file-loader'
+//      },
+//      {
+//        test:   /\.(png|jpg)$/,
+//        loader: 'url-loader?limit=25000',
+//      },
+//      {
+//        test:   /\.(woff|woff2|ttf|eot)$/,
+//        loader: 'url-loader?limit=50000',
+//      },
+//      {
+//        test:   /\.svg$/,
+//        loader: 'file-loader',
+//      },
+{
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url-loader?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file-loader"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+    },
+    {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=25000'
+    }
     ]
   },
 
@@ -62,8 +76,6 @@ var commonConfig = {
       inject:   'body',
       title:    'Onboarding Wizard'
     }),
-
-    new ExtractTextPlugin('vendor.[chunkhash].css'),
   ],
 
 }
@@ -80,10 +92,30 @@ if ( TARGET_ENV === 'development' ) {
     ],
 
     devtool: 'eval-source-map',
+
+    // suppress perf hints in browser devtools
+    performance: { hints: false },
     
     devServer: {
       // serve index.html in place of 404 responses
       historyApiFallback: true,
+
+      stats: {
+        colors: true,
+        hash: false,
+        version: false,
+        timings: false,
+        assets: false,
+        chunks: false,
+        modules: false,
+        reasons: false,
+        children: false,
+        source: false,
+        errors: true,
+        errorDetails: false,
+        warnings: false,
+        publicPath: false
+      }
     },
 
     module: {
@@ -91,7 +123,11 @@ if ( TARGET_ENV === 'development' ) {
         {
           test:    /\.elm$/,
           exclude: [/elm-stuff/, /node_modules/],
-          loader:  'elm-hot-loader!elm-webpack-loader?verbose=true&warn=true&debug=true'
+          loader:  'elm-hot-loader!elm-webpack-loader?verbose=true&warn=true&debug=true',
+        },
+        {
+          test:   /\.css$/i,
+          loader: 'style-loader!css-loader?sourceMap',
         },
       ]
     }
@@ -113,24 +149,37 @@ if ( TARGET_ENV === 'production' ) {
           exclude: [/elm-stuff/, /node_modules/],
           loader:  'elm-webpack-loader'
         },
+        {
+          test:   /\.css$/i,
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: 'style-loader',
+            loader: 'css-loader'
+          }),          
+        },
       ]
     },
 
     plugins: [
+      new CleanDistPlugin('dist', {
+        root: __dirname
+      }),
+
+      new ExtractTextPlugin(vendorCSSName),
+
       // minify & mangle JS/CSS
       new webpack.optimize.UglifyJsPlugin({
           minimize:   true,
           compressor: { warnings: false }
           // mangle:  true
       }),
-      new CleanDistPlugin('dist', {
-        root: __dirname
-      }),
+
       new PurifyCSSPlugin({
         basePath: outputPath,
         paths: ['index.html']
       }),
-      new OptimizeCSSPlugin()
-    ]
+      new OptimizeCSSPlugin({
+        cssProcessorOptions: { discardComments: { removeAll: true }},
+      }),
+    ],
   });
 }
